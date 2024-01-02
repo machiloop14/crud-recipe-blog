dd334848f9fcdb1ad93feb30a97bd841e7dcc3e8
hamidu sodiq <hamidusodiq2@gmail.com>
2024-01-02 16:31:34 +0100

    validate and upload form data to database

--- src/components/AddRecipe.tsx
+++ src/components/AddRecipe.tsx
@@ -1,66 +1,10 @@
-import * as MdIcons from "react-icons/md";
+import { RecipeForm } from "./RecipeForm";
 
 const AddRecipe = () => {
   return (
     <div>
       <h1 className="text-4xl">Add Recipe</h1>
-      <form className="flex flex-col gap-4 text-sm mt-8">
-        <div className="form__field grid ">
-          <label htmlFor="recipeTitle" className="w-36">
-            Recipe Title:
-          </label>
-          <input
-            type="text"
-            id="recipeTitle"
-            className="border outline-0 border-gray-500 focus:border-blue-600 px-1 py-1 h-10"
-          />
-        </div>
-        <div className="form__field grid ">
-          <label htmlFor="recipeImage" className="w-36">
-            Recipe Image url:
-          </label>
-          <input
-            type="url"
-            id="recipeImage"
-            className="border outline-0 border-gray-500 focus:border-blue-600 px-1 py-1 h-10"
-          />
-        </div>
-        <div className="form__field grid ">
-          <label htmlFor="recipeDescription">Recipe Description: </label>
-          <textarea
-            name="recipeDescription"
-            id="recipeDescription"
-            className="border outline-0 border-gray-500 focus:border-blue-600 px-1 py-1 h-28 resize-none"
-          ></textarea>
-        </div>
-        <div className="form__field grid ">
-          <label htmlFor="recipeIngredients">Recipe Ingredients: </label>
-          <textarea
-            name="recipeIngredients"
-            id="recipeIngredients"
-            className="border outline-0 border-gray-500 focus:border-blue-600 px-1 py-1 h-28 resize-none"
-          ></textarea>
-        </div>
-        <div className="form__field grid ">
-          <label htmlFor="recipeInstruction">Instruction 1: </label>
-          <textarea
-            name="recipeInstruction"
-            id="recipeInstruction"
-            className="border outline-0 border-gray-500 focus:border-blue-600 px-1 py-1 h-14 resize-none"
-          ></textarea>
-        </div>
-        <button className="flex items-center gap-1">
-          <MdIcons.MdAddCircle
-            style={{ color: "black", width: "30px", height: "30px" }}
-          />
-          <span>Add Instruction</span>
-        </button>
-        <input
-          type="submit"
-          className="bg-gray-500 text-white p-2 mt-3 cursor-pointer hover:bg-black"
-          value="submit"
-        />
-      </form>
+      <RecipeForm />
     </div>
   );
 };
---
+++ src/components/RecipeForm.tsx
@@ -0,0 +1,146 @@
+import * as MdIcons from "react-icons/md";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+
+interface RecipeFormData {
+  title: string;
+  imageUrl: string;
+  description: string;
+  ingredients: string;
+  instruction: string;
+}
+
+export const RecipeForm = () => {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  const schema = yup.object().shape({
+    title: yup.string().required("You must add a title."),
+    imageUrl: yup.string(),
+    description: yup.string().required("You must add a description"),
+    ingredients: yup.string().required("You must add a list of ingredients"),
+    instruction: yup.string().required("You must add at least one instruction"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RecipeFormData>({
+    resolver: yupResolver(schema),
+  });
+
+  const recipesRef = collection(db, "recipe");
+
+  //   const onCreateRecipe = async (data: RecipeFormData) => {
+  //     await addDoc(recipesRef, {
+  //       title: data.title,
+  //       imageUrl: data.imageUrl,
+  //       description: data.description,
+  //       ingredients: data.ingredients,
+  //       instruction: data.instruction,
+  //       author: user?.displayName,
+  //       id: user?.uid,
+  //     });
+  //
+  //    navigate("/")
+  //   };
+
+  const onCreateRecipe = async (data: RecipeFormData) => {
+    await addDoc(recipesRef, {
+      ...data,
+      author: user?.displayName,
+      userId: user?.uid,
+    });
+
+    navigate("/");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onCreateRecipe)}
+      className="flex flex-col gap-4 text-sm mt-8"
+    >
+      <div className="form__field grid ">
+        <label htmlFor="recipeTitle" className="w-36">
+          Recipe Title:
+        </label>
+        <div>
+          <input
+            {...register("title")}
+            id="recipeTitle"
+            className="border outline-0 border-gray-500 focus:border-blue-600 px-1 py-1 h-10 w-full"
+          />
+          <p className="text-red-600">{errors.title?.message}</p>
+        </div>
+      </div>
+
+      <div className="form__field grid ">
+        <label htmlFor="recipeImage" className="w-36">
+          Recipe Image url:
+        </label>
+        <div>
+          <input
+            {...register("imageUrl")}
+            id="recipeImage"
+            className="border outline-0 border-gray-500 focus:border-blue-600 px-1 py-1 h-10 w-full"
+          />
+          <p className="text-red-600">{errors.imageUrl?.message}</p>
+        </div>
+      </div>
+
+      <div className="form__field grid ">
+        <label htmlFor="recipeDescription">Recipe Description: </label>
+        <div>
+          <textarea
+            {...register("description")}
+            id="recipeDescription"
+            className="border outline-0 border-gray-500 focus:border-blue-600 px-1 py-1 h-28 resize-none w-full"
+          ></textarea>
+          <p className="text-red-600">{errors.description?.message}</p>
+        </div>
+      </div>
+
+      <div className="form__field grid ">
+        <label htmlFor="recipeIngredients">Recipe Ingredients: </label>
+        <div>
+          <textarea
+            {...register("ingredients")}
+            id="recipeIngredients"
+            className="border outline-0 border-gray-500 focus:border-blue-600 px-1 py-1 h-28 resize-none w-full"
+          ></textarea>
+          <p className="text-red-600">{errors.ingredients?.message}</p>
+        </div>
+      </div>
+
+      <div className="form__field grid ">
+        <label htmlFor="recipeInstruction">Instruction 1: </label>
+        <div>
+          <textarea
+            {...register("instruction")}
+            id="recipeInstruction"
+            className="border outline-0 border-gray-500 focus:border-blue-600 px-1 py-1 h-14 resize-none w-full"
+          ></textarea>
+          <p className="text-red-600">{errors.instruction?.message}</p>
+        </div>
+      </div>
+
+      <button className="flex items-center gap-1">
+        <MdIcons.MdAddCircle
+          style={{ color: "black", width: "30px", height: "30px" }}
+        />
+        <span>Add Instruction</span>
+      </button>
+      <input
+        type="submit"
+        className="bg-gray-500 text-white p-2 mt-3 cursor-pointer hover:bg-black"
+        value="submit"
+      />
+    </form>
+  );
+};
