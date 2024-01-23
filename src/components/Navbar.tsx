3dc9b173a1402291962ef1d504ae371c5fecb0ef
hamidu sodiq <hamidusodiq2@gmail.com>
2024-01-23 22:42:49 +0100

    make navigation mobile responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,12 @@ import { auth } from "../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { signOut } from "firebase/auth";
 import useNotification from "../customHooks/useNotification";
+import { useState } from "react";
 
 const Navbar = () => {
   const [user] = useAuthState(auth);
   const { notify } = useNotification();
+  const [hamburgerMenu, setHamburgerMenu] = useState<boolean>(false);
   const navigate = useNavigate();
   const signUserOut = async () => {
     await signOut(auth);
@@ -16,6 +18,12 @@ const Navbar = () => {
     navigate("/");
     notify("Logout successful", { type: "success" });
   };
+
+  const toggleVisiblity = () => {
+    setHamburgerMenu((prev) => !prev);
+  };
+
+  console.log(hamburgerMenu);
 
   return (
     <header className="flex justify-between items-center py-5 border-solid border-b border-black">
@@ -44,16 +52,37 @@ const Navbar = () => {
       </div>
 
       {/* right side of navigation */}
-      <div className="right-nav flex gap-4 items-center ">
-        <NavLink to="/" className="hover:text-blue-600 ">
-          All Recipes
-        </NavLink>
-        <NavLink to="/add-recipe" className="hover:text-blue-600">
-          Add Recipe
-        </NavLink>
-        <NavLink to="/bookmarked" className="hover:text-blue-600">
-          Bookmarked Recipes
-        </NavLink>
+      <div className="flex items-center">
+        <div
+          className={`right-nav md:flex flex-col md:flex-row gap-4 items-center fixed md:static right-0 top-0 bg-white bottom-0 pt-16  md:pt-0 w-60 md:w-auto ${
+            hamburgerMenu ? "flex" : "hidden"
+          }`}
+        >
+          <NavLink to="/" className="hover:text-blue-600 ">
+            All Recipes
+          </NavLink>
+          <NavLink to="/add-recipe" className="hover:text-blue-600">
+            Add Recipe
+          </NavLink>
+          <NavLink to="/bookmarked" className="hover:text-blue-600">
+            Bookmarked Recipes
+          </NavLink>
+        </div>
+        <button onClick={toggleVisiblity}>
+          {hamburgerMenu ? (
+            <MdIcons.MdClose
+              className={`md:hidden z-50 w-8 h-8 top-4 right-0 mr-4 ${
+                hamburgerMenu ? "fixed" : "absolute"
+              }`}
+            />
+          ) : (
+            <MdIcons.MdMenu
+              className={`md:hidden z-50 w-8 h-8 top-4 right-0 mr-4 ${
+                hamburgerMenu ? "fixed" : "absolute"
+              }`}
+            />
+          )}
+        </button>
       </div>
     </header>
   );
