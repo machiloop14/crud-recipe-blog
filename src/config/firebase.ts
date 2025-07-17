de3c7b666daacc547c246ad1499791b06cfdd246
machiloop <hamidusodiq2@gmail.com>
2025-07-17 09:03:07 +0100

    fix bug with firebase authentication

---
+++ src/config/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "REDACTED",
+  authDomain: "recipe-app-9045e.firebaseapp.com",
+  projectId: "recipe-app-9045e",
+  storageBucket: "recipe-app-9045e.firebasestorage.app",
+  messagingSenderId: "375286968590",
+  appId: "1:375286968590:web:d761bed2f3323a4a86275f",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
